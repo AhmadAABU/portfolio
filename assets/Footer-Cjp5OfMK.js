9b56d955db6ced4f0555a20205e7e5e144d07bc6
AhmadAABU <AhmadAABU@users.noreply.github.com>
2025-09-17 23:22:10 +0000

    deploy: 16af9b078e07179d6c26235c15d717a85b38fd1d

--- assets/Footer-Cjp5OfMK.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-DiPfh4OT.js";function r(){return e.jsx("div",{className:"footer",children:e.jsx("h3",{className:"footer-p",children:"© 2025 Ahmad Jawdat"})})}export{r as default};
---
+++ assets/Mainbtn-I23EN-hp.js
@@ -0,0 +1,1 @@
+import{j as s}from"./index-CjpKieiC.js";function i({title:n,id:r}){return s.jsx("div",{children:s.jsx("h1",{className:"sec-h",id:r,children:n})})}export{i as M};
