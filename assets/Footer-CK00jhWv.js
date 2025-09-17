8f3bba3d9ccaa4451129c20ce214955be2736e2a
Ahmad Jawdat <Ahmad.jawdat@mail.ru>
2025-09-18 01:35:02 +0300

    Updates

---
+++ assets/Footer-CK00jhWv.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-DKWzZKkh.js";function r(){return e.jsx("div",{className:"footer",children:e.jsx("h3",{className:"footer-p",children:"© 2025 Ahmad Jawdat"})})}export{r as default};
